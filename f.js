6d820d819e9b2b1d129459f1af81c383598b888c
fka12354 <gabriel.rotelalopez@gmail.com>
2025-10-28 20:46:43 -0300

    Refactor llevar function to remove unnecessary code

--- f.js
+++ f.js
@@ -1,11 +1,9 @@
-function llevar(Opcion) {
-  if (Opcion === "Informacion") {
-    window.location.href = "index.html";
-  } else if (Opcion === "Proyectos") {
-    window.location.href = "proyecto.html";}
-  else if (Opcion === "Hobbies") {
-    window.location.href = "hobbies.html";
-  } else if (Opcion === "Contacto") {
-    window.location.href = "contacto.html";
-  }
+if (Opcion === "Informacion") {
+  window.location.href = "index.html";
+} else if (Opcion === "Proyectos") {
+  window.location.href = "proyecto.html";
+} else if (Opcion === "Hobbies") {
+  window.location.href = "hobbies.html";
+} else if (Opcion === "Contacto") {
+  window.location.href = "contacto.html";
 }
