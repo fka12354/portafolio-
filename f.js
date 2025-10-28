c7a1f066b23b658318c8cd7c7b8658c738fc99c2
fka12354 <gabriel.rotelalopez@gmail.com>
2025-10-28 20:30:48 -0300

    first commit

---
+++ f.js
@@ -0,0 +1,11 @@
+function llevar(Opcion) {
+  if (Opcion === "Informacion") {
+    window.location.href = "folio.html";
+  } else if (Opcion === "Proyectos") {
+    window.location.href = "proyecto.html";}
+  else if (Opcion === "Hobbies") {
+    window.location.href = "hobbies.html";
+  } else if (Opcion === "Contacto") {
+    window.location.href = "contacto.html";
+  }
+}
