c12e1100a7112584b4974e11f6b47b1a5576f89c
fka12354 <gabriel.rotelalopez@gmail.com>
2025-10-28 20:52:37 -0300

    Refactor navigation logic into llevar function

--- f.js
+++ f.js
@@ -1,9 +1,11 @@
-if (Opcion === "Informacion") {
-  window.location.href = "index.html";
-} else if (Opcion === "Proyectos") {
-  window.location.href = "proyecto.html";
-} else if (Opcion === "Hobbies") {
-  window.location.href = "hobbies.html";
-} else if (Opcion === "Contacto") {
-  window.location.href = "contacto.html";
+function llevar(Opcion) {
+  if (Opcion === "Informacion") {
+    window.location.href = "folio.html";
+  } else if (Opcion === "Proyectos") {
+    window.location.href = "proyecto.html";}
+  else if (Opcion === "Hobbies") {
+    window.location.href = "hobbies.html";
+  } else if (Opcion === "Contacto") {
+    window.location.href = "contacto.html";
+  }
 }
